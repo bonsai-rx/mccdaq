ddf17cdf16a0fd6e513004c8dc935c28cb28c88d
glopesdev <glopesdev@users.noreply.github.com>
2020-04-24 21:23:18 +0100

    Add support for sampling individual analog inputs

---
+++ Bonsai.MccDaq/AnalogInput.cs
@@ -0,0 +1,38 @@
+﻿using MccDaq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.MccDaq
+{
+    public class AnalogInput : Combinator<short>
+    {
+        public int BoardNumber { get; set; }
+
+        public int Channel { get; set; }
+
+        public global::MccDaq.Range Range { get; set; }
+
+        public override IObservable<short> Process<TSource>(IObservable<TSource> source)
+        {
+            return Observable.Defer(() =>
+            {
+                var range = Range;
+                var channel = Channel;
+                var board = new MccBoard(BoardNumber);
+                return source.Select(input =>
+                {
+                    var error = board.AIn(channel, range, out short dataValue);
+                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+                    {
+                        throw new InvalidOperationException(error.Message);
+                    }
+                    return dataValue;
+                });
+            });
+        }
+    }
+}
