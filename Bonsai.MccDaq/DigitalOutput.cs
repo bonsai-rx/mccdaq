d477655d12c7b069dc5947c1cbef1a4e9e7bdd9d
glopesdev <glopesdev@users.noreply.github.com>
2020-04-25 17:48:56 +0100

    Add support for single bit digital I/O

--- Bonsai.MccDaq/DigitalOutput.cs
+++ Bonsai.MccDaq/DigitalOutput.cs
@@ -5,34 +5,51 @@ using System.Reactive.Linq;
 
 namespace Bonsai.MccDaq
 {
-    [Description("Writes a byte to a digital I/O port in a Measurement Computing device.")]
+    [Description("Sets the state of a digital I/O port in a Measurement Computing device.")]
     public class DigitalOutput : Sink<short>
     {
         [Description("The board number as defined in the Instacal system config file.")]
         public int BoardNumber { get; set; }
 
+        [Description("The optional specific digital bit to write on the I/O port. If no bit is specified, the entire port state is set.")]
+        public int? BitNumber { get; set; }
+
         [Description("The number of the I/O port to write.")]
         public DigitalPortType PortType { get; set; } = DigitalPortType.AuxPort;
+
+        static void ThrowExceptionForErrorInfo(ErrorInfo error)
+        {
+            if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+            {
+                throw new InvalidOperationException(error.Message);
+            }
+        }
 
         public override IObservable<short> Process(IObservable<short> source)
         {
             return Observable.Defer(() =>
             {
+                ErrorInfo configError;
                 var portType = PortType;
+                var bitNumber = BitNumber;
                 var board = new MccBoard(BoardNumber);
-                var configError = board.DConfigPort(portType, DigitalPortDirection.DigitalOut);
-                if (configError.Value != ErrorInfo.ErrorCode.NoErrors)
-                {
-                    throw new InvalidOperationException(configError.Message);
-                }
+                if (bitNumber.HasValue) configError = board.DConfigBit(portType, bitNumber.Value, DigitalPortDirection.DigitalOut);
+                else configError = board.DConfigPort(portType, DigitalPortDirection.DigitalOut);
+                ThrowExceptionForErrorInfo(configError);
 
-                return source.Do(dataValue =>
+                if (bitNumber.HasValue)
+                {
+                    return source.Do(input =>
+                    {
+                        var bitValue = input != 0 ? DigitalLogicState.High : DigitalLogicState.Low;
+                        var error = board.DBitOut(portType, bitNumber.Value, bitValue);
+                        ThrowExceptionForErrorInfo(error);
+                    });
+                }
+                else return source.Do(dataValue =>
                 {
                     var error = board.DOut(portType, dataValue);
-                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
-                    {
-                        throw new InvalidOperationException(error.Message);
-                    }
+                    ThrowExceptionForErrorInfo(error);
                 });
             });
         }
--- Bonsai.MccDaq/SampleDigital.cs
+++ Bonsai.MccDaq/SampleDigital.cs
@@ -7,34 +7,51 @@ using System.Reactive.Linq;
 namespace Bonsai.MccDaq
 {
     [Description("Samples a digital I/O port in a Measurement Computing device.")]
-    public class SampleDigital : Combinator<int>
+    public class SampleDigital : Combinator<short>
     {
         [Description("The board number as defined in the Instacal system config file.")]
         public int BoardNumber { get; set; }
 
+        [Description("The optional specific digital bit to read from the I/O port. If no bit is specified, the entire port state is sampled.")]
+        public int? BitNumber { get; set; }
+
         [Description("The number of the I/O port to read.")]
         public DigitalPortType PortType { get; set; } = DigitalPortType.AuxPort;
 
-        public override IObservable<int> Process<TSource>(IObservable<TSource> source)
+        static void ThrowExceptionForErrorInfo(ErrorInfo error)
+        {
+            if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+            {
+                throw new InvalidOperationException(error.Message);
+            }
+        }
+
+        public override IObservable<short> Process<TSource>(IObservable<TSource> source)
         {
             return Observable.Defer(() =>
             {
+                ErrorInfo configError;
                 var portType = PortType;
+                var bitNumber = BitNumber;
                 var board = new MccBoard(BoardNumber);
-                var configError = board.DConfigPort(portType, DigitalPortDirection.DigitalIn);
-                if (configError.Value != ErrorInfo.ErrorCode.NoErrors)
-                {
-                    throw new InvalidOperationException(configError.Message);
-                }
+                if (bitNumber.HasValue) configError = board.DConfigBit(portType, bitNumber.Value, DigitalPortDirection.DigitalIn);
+                else configError = board.DConfigPort(portType, DigitalPortDirection.DigitalIn);
+                ThrowExceptionForErrorInfo(configError);
 
-                return source.Select(input =>
+                if (bitNumber.HasValue)
+                {
+                    return source.Select(input =>
+                    {
+                        var error = board.DBitIn(portType, bitNumber.Value, out DigitalLogicState bitValue);
+                        ThrowExceptionForErrorInfo(error);
+                        return (short)bitValue;
+                    });
+                }
+                else return source.Select(input =>
                 {
                     var error = board.DIn(portType, out short dataValue);
-                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
-                    {
-                        throw new InvalidOperationException(error.Message);
-                    }
-                    return (int)dataValue;
+                    ThrowExceptionForErrorInfo(error);
+                    return dataValue;
                 });
             });
         }
