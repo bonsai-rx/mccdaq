f77239fed2c714ea0cd2064a0b85d7fa17fa520b
glopesdev <glopesdev@users.noreply.github.com>
2020-04-25 16:39:42 +0100

    Add support for digital and analog output

---
+++ Bonsai.MccDaq/AnalogOutput.cs
@@ -0,0 +1,42 @@
+﻿using MccDaq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.MccDaq
+{
+    public class AnalogOutput : Sink<short>
+    {
+        public AnalogOutput()
+        {
+            Range = global::MccDaq.Range.NotUsed;
+        }
+
+        public int BoardNumber { get; set; }
+
+        public int Channel { get; set; }
+
+        public global::MccDaq.Range Range { get; set; }
+
+        public override IObservable<short> Process(IObservable<short> source)
+        {
+            return Observable.Defer(() =>
+            {
+                var range = Range;
+                var channel = Channel;
+                var board = new MccBoard(BoardNumber);
+                return source.Do(dataValue =>
+                {
+                    var error = board.AOut(channel, range, dataValue);
+                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+                    {
+                        throw new InvalidOperationException(error.Message);
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ Bonsai.MccDaq/DigitalOutput.cs
@@ -0,0 +1,40 @@
+﻿using MccDaq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.MccDaq
+{
+    public class DigitalOutput : Sink<short>
+    {
+        public int BoardNumber { get; set; }
+
+        public DigitalPortType PortType { get; set; } = DigitalPortType.AuxPort;
+
+        public override IObservable<short> Process(IObservable<short> source)
+        {
+            return Observable.Defer(() =>
+            {
+                var portType = PortType;
+                var board = new MccBoard(BoardNumber);
+                var configError = board.DConfigPort(portType, DigitalPortDirection.DigitalOut);
+                if (configError.Value != ErrorInfo.ErrorCode.NoErrors)
+                {
+                    throw new InvalidOperationException(configError.Message);
+                }
+
+                return source.Do(dataValue =>
+                {
+                    var error = board.DOut(portType, dataValue);
+                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+                    {
+                        throw new InvalidOperationException(error.Message);
+                    }
+                });
+            });
+        }
+    }
+}
