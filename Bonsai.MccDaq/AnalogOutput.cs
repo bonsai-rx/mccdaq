4d27866e82ca1f7291c8b6f5eefa8c0725b2cb04
glopesdev <glopesdev@users.noreply.github.com>
2020-04-26 13:48:09 +0100

    Add support for multi-channel analog output

--- Bonsai.MccDaq/AnalogOutput.cs
+++ Bonsai.MccDaq/AnalogOutput.cs
@@ -1,11 +1,13 @@
 ﻿using MccDaq;
+using OpenCV.Net;
 using System;
 using System.ComponentModel;
+using System.Reactive;
 using System.Reactive.Linq;
 
 namespace Bonsai.MccDaq
 {
-    [Description("Sets the value of an analog output channel in a Measurement Computing device.")]
+    [Description("Sets the value of one or more analog output channels in a Measurement Computing device.")]
     public class AnalogOutput : Sink<short>
     {
         public AnalogOutput()
@@ -22,6 +24,14 @@ namespace Bonsai.MccDaq
         [Description("Specifies the range used in the D/A device. If the board has a fixed gain, this parameter is ignored.")]
         public global::MccDaq.Range Range { get; set; }
 
+        static void ThrowExceptionForErrorInfo(ErrorInfo error)
+        {
+            if (error.Value != ErrorInfo.ErrorCode.NoErrors)
+            {
+                throw new InvalidOperationException(error.Message);
+            }
+        }
+
         public override IObservable<short> Process(IObservable<short> source)
         {
             return Observable.Defer(() =>
@@ -32,11 +42,40 @@ namespace Bonsai.MccDaq
                 return source.Do(dataValue =>
                 {
                     var error = board.AOut(channel, range, dataValue);
-                    if (error.Value != ErrorInfo.ErrorCode.NoErrors)
-                    {
-                        throw new InvalidOperationException(error.Message);
-                    }
+                    ThrowExceptionForErrorInfo(error);
                 });
+            });
+        }
+
+        public IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return Observable.Create<Mat>(observer =>
+            {
+                var range = Range;
+                var lowChannel = Channel;
+                var board = new MccBoard(BoardNumber);
+                var options = ScanOptions.Default;
+                var scanObserver = Observer.Create<Mat>(
+                    buffer =>
+                    {
+                        var actualRate = 0;
+                        var highChannel = lowChannel + buffer.Rows - 1;
+                        var error = board.AOutScan(lowChannel, highChannel, buffer.Rows, ref actualRate, range, buffer.Data, options);
+                        try
+                        {
+                            ThrowExceptionForErrorInfo(error);
+                            if (actualRate != 0)
+                            {
+                                throw new InvalidOperationException($"The specified sampling rate is not available. Suggested rate: {actualRate}");
+                            }
+
+                            observer.OnNext(buffer);
+                        }
+                        catch (Exception ex) { observer.OnError(ex); }
+                    },
+                    observer.OnError,
+                    observer.OnCompleted);
+                return source.SubscribeSafe(scanObserver);
             });
         }
     }
